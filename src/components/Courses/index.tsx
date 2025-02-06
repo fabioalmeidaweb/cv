7048f30af6758f25b10f7078eef79dff6888c8fa
Fabio Almeida <fabio.natito@gmail.com>
2025-02-06 10:42:18 -0300

    (bugfix) fix break blocks inside courses

--- src/components/Courses/index.tsx
+++ src/components/Courses/index.tsx
@@ -1,18 +1,20 @@
-import { CoursesType } from "@/types";
+import { CoursesType } from '@/types';
+
 type CoursesProps = {
   courses: CoursesType;
 };
+
 export default function Courses({ courses }: CoursesProps) {
   return (
-    <section className="break-inside-avoid">
+    <section>
       <h2 className="my-8 text-3xl uppercase border-b border-zinc-800">{courses.label}</h2>
-        {courses.values.map((item) => (
-          <div className="my-5" key={item.title}>
-            <h3 className="text-lg font-semibold pb-1">{item.title}</h3>
-            <h4 className="font-semibold break-words">{item.where}</h4>
-            <p className="break-words text-zinc-500">{item.year}</p>
-          </div>
-        ))}
+      {courses.values.map((item) => (
+        <div className="my-5 break-inside-avoid" key={item.title}>
+          <h3 className="text-lg font-semibold pb-1">{item.title}</h3>
+          <h4 className="font-semibold break-words">{item.where}</h4>
+          <p className="break-words text-zinc-500">{item.year}</p>
+        </div>
+      ))}
     </section>
   );
 }
